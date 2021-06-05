23e14f71b1b3abeb9525b3970ecbf8149c76be47
garima3012 <85414081+garima3012@users.noreply.github.com>
2021-06-06 01:13:10 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <i>
+          <b>SHAPEAI</b>
+        </i>
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h3> Web development </h3>
+      <p>
+        {" "}
+        It was an amazing bootcamp. I have learnt so much from this bootcamp.I
+        consider myself lucky that I was a part of this bootcamp.Shaurya bhaiya
+        is an amazing teacher.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
